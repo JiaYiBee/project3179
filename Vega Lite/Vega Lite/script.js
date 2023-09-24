867b437ac3c2ded1a2b82c90a3d7c55d60291676
JiaYiBee <112955736+JiaYiBee@users.noreply.github.com>
2023-09-24 21:19:15 +0800

    Add files via upload

---
+++ Vega Lite/Vega Lite/script.js
@@ -0,0 +1,5 @@
+var graph_1 = "graph.json";
+
+vegaEmbed("#bar_chart", graph_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
