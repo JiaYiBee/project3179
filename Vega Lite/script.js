5de8a5ec29b834d67ed35589cebf7e9776aa4481
JiaYiBee <112955736+JiaYiBee@users.noreply.github.com>
2023-10-11 23:37:46 +0800

    Update script.js

--- Vega Lite/script.js
+++ Vega Lite/script.js
@@ -1,6 +1,5 @@
 var graph_1 = "graph.json";
-var graph_4 = "graphinteractive2.json";
-var graph_5 = "Debug.json";
+var graph_4 = "debug.json";
 
 vegaEmbed("#map", graph_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -10,6 +9,3 @@ vegaEmbed("#map4", graph_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#map2", graph_5).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
