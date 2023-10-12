93f70b5f0c36b23a7140177c59162f754784b111
JiaYiBee <112955736+JiaYiBee@users.noreply.github.com>
2023-10-13 02:46:25 +0800

    Add files via upload

---
+++ 3179Final/script.js
@@ -0,0 +1,84 @@
+// var graph_1 = "graph.json";
+// var graph_2 = "graph2.json";
+// var graph_3 = "graphtest.json";
+// var graph_4 = "graphinteractive2.json";
+
+// vegaEmbed("#map", graph_1).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// vegaEmbed("#map2", graph_2).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// vegaEmbed("#map3", graph_3).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// vegaEmbed("#map4", graph_4).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+var a2graph1 = "a2_1.json";
+var a2graph2 = "a2_2.json";
+var a2graph3 = "a2_3.json";
+var a2graph4 = "a2_4.json";
+var a2graph5 = "a2_5.json";
+var a2graph6 = "a2_6.json";
+var a2graph7 = "a2_7.json";
+var a2graph8 = "a2_8.json";
+var a2graph9 = "a2_9.json";
+
+vegaEmbed("#map1", a2graph1)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#map2", a2graph2)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#map3", a2graph3)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#map4", a2graph4)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#map5", a2graph5)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#map6", a2graph6)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#map7", a2graph7)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#map8", a2graph8)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#map9", a2graph9)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
