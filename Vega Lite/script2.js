b7eef20d916acad95e3e91057d45aacec2eb6e7f
JiaYiBee <112955736+JiaYiBee@users.noreply.github.com>
2023-10-12 12:06:52 +0800

    Create script2.js

---
+++ Vega Lite/script2.js
@@ -0,0 +1,7 @@
+var co2.json = "graph2"
+
+vegaEmbed("#map", graph2)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
